5287820e06992e1e44c5c730099f578f55b5700d
AdrianP <apetresc@gmail.com>
2010-08-25 21:25:33 -0400

    Added JavaDoc to KWTProgressBar. Also added missing copyright notices to everything.

--- src/org/kwt/ui/KWTProgressBar.java
+++ src/org/kwt/ui/KWTProgressBar.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2010 Adrian Petrescu.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located in the LICENSE file included with this
+ * distribution.
+ * 
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the
+ * License.
+ */
+
 package org.kwt.ui;
 
 import java.awt.Color;
@@ -9,7 +23,8 @@ import org.apache.log4j.Logger;
 import com.amazon.kindle.kindlet.ui.KComponent;
 
 /**
- * A progress bar.
+ * A progress bar. Represents the proportion of progress towards completing a particular task. Meant
+ * to emulate the book progress bar at the bottom of the regular Kindle reader application.
  * 
  * @author Adrian Petrescu
  *
@@ -26,42 +41,80 @@ public class KWTProgressBar extends KComponent {
     private int totalTicks;
     private int currentTick;
     
+    /** 
+     * Constructs a new progress bar, with 100 total ticks. The current tick begins at 0.
+     */
     public KWTProgressBar() {
         this(100);
     }
     
+    /**
+     * Constructs a new progress bar. The current tick begins at 0.
+     * 
+     * @param totalTicks The total number of ticks representing completion of the task.
+     */
     public KWTProgressBar(int totalTicks) {
         this.totalTicks = totalTicks;
         this.currentTick = 0;
     }
     
+    /**
+     * Returns the total number of ticks.
+     * 
+     * @return the total number of ticks.
+     */
     public int getTotalTicks() {
         return totalTicks;
     }
     
+    /**
+     * Returns the current tick.
+     * 
+     * @return the current tick.
+     */
     public int getCurrentTick() {
         return currentTick;
     }
     
+    /**
+     * Sets the current tick. If <code>currentTick < 0</code> or 
+     * <code>currentTick > totalTicks</code>, then <code>currentTick</code>
+     * will be set to 0 or <code>totalTicks</code> respectively.
+     * 
+     * @param currentTick the current tick.
+     */
     public void setCurrentTick(int currentTick) {
-        this.currentTick = currentTick;
+        this.currentTick = Math.min(totalTicks, Math.max(0, currentTick));
     }
     
+    /**
+     * {@inheritDoc }
+     */
     public Dimension getPreferredSize() {
         Dimension d = getMinimumSize();
         d.width = Math.max(width, d.width);
         return d;
     }
     
+    /**
+     * {@inheritDoc }
+     */
     public Dimension getMinimumSize() {
         return new Dimension(getFontMetrics(getFont()).stringWidth("100%") + 2 * HORIZONTAL_PADDING,
                 getFontMetrics(getFont()).getHeight() + 2 * VERTICAL_PADDING);
     }
     
+    /**
+     * Sets the width (in pixels) of this component.
+     * @param width the width (in pixels) of this component.
+     */
     public void setWidth(int width) {
         this.width = width;
     }
     
+    /**
+     * {@inheritDoc }
+     */
     public void paint(Graphics g) {
         double progress = (double) currentTick / totalTicks;
         logger.info("Progress: " + progress);
--- src/org/kwt/ui/KWTScrollPanel.java
+++ src/org/kwt/ui/KWTScrollPanel.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2010 Adrian Petrescu.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located in the LICENSE file included with this
+ * distribution.
+ * 
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the
+ * License.
+ */
+
 package org.kwt.ui;
 
 import java.awt.Dimension;
